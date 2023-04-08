f048e0322fad0ec3ac000eb337a935c98c5b43fd
Esmyrna <Esmyrnaoliveira13@gmail.com>
2023-04-08 16:38:01 -0300

    criando projeto

---
+++ src/global.tsx
@@ -0,0 +1,16 @@
+import { createGlobalStyle } from "styled-components";
+
+
+export const GlobalStyle = createGlobalStyle`
+    body {
+    margin: 0;
+    padding: 0;
+    background-color: #d6d4d4;
+    font-size: 1rem;
+
+    height: 100vh;
+    width: 100vw;
+ 
+    }
+
+`
