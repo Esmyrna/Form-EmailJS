179140c141ebe98609d9ad8be9a25190a701caba
JoaoVNicacio <joaonicks99@gmail.com>
2023-04-16 22:09:17 -0300

    Adição da lógica de envio do email e criação do textarea de mensagem

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,18 +1,70 @@
-import React from 'react'
-import { Form, InputName, LabelName } from './style'
+import React, { useState } from 'react';
+import { Form, InputName, LabelName, MessageInput, SubmitButton } from './style';
+import emailjs from 'emailjs-com';
 
 const Input = () => {
+
+  const SERVICE_ID = 'gmailForm';
+  const TEMPLATE_ID = 'template_729ximg';
+  const USER_ID = 'o_8iMEK5GWeQaAmR5';
+
+  const handleFormSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(fields);
+
+    const templateParams = {
+      from_name: fields.name,
+      email: fields.email,
+      to_name: "Esmyrna",
+      to_email: "esmyrna.oliveira@gmail.com",
+      subject: fields.subject,
+      message: fields.message.replace(/\n/g, "<br>") // Converte quebras de linha em <br> para manter o formato do texto
+    };
+
+    // Envia o e-mail
+    emailjs.send(SERVICE_ID, TEMPLATE_ID, templateParams, USER_ID)
+      .then((response) => {
+        console.log('E-mail enviado com sucesso!', response.status, response.text);
+      }, (error) => {
+        console.log('Erro ao enviar e-mail:', error);
+      });
+  }
+
+  const [fields, setFields] = useState({
+    name: '',
+    email: '',
+    subject: '',
+    message: '',
+  });
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFields({
+      ...fields,
+      [e.target.name]: e.target.value
+    });
+  };
+
+  const handleMessageChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setFields({
+      ...fields,
+      message: e.target.value
+    });
+  };
+
   return (
-    <Form>
-      <LabelName>Nome </LabelName>
-      <InputName width={"80%"} type="name" required></InputName>
+    <Form onSubmit={handleFormSubmit}>
+      <LabelName>Nome</LabelName>
+      <InputName width={"80%"} type="name" name="name" onChange={handleInputChange} required></InputName>
       <LabelName>E-mail </LabelName>
-      <InputName width={"80%"} type="email" required></InputName>
+      <InputName width={"80%"} type="email" name="email" onChange={handleInputChange} required></InputName>
       <LabelName>Assunto </LabelName>
-      <InputName width={"50%"} type="text" required></InputName>
+      <InputName width={"80%"} type="text" name="subject" onChange={handleInputChange} required></InputName>
+      <LabelName>Mensagem </LabelName>
+      <MessageInput required onChange={handleMessageChange} />
 
+      <SubmitButton type="submit">Enviar e-mail</SubmitButton>
     </Form>
-  )
-}
+  );
+};
 
-export default Input
+export default Input;
--- src/components/Input/style.tsx
+++ src/components/Input/style.tsx
@@ -19,6 +19,39 @@ font-size: 1rem;
 font-family: 'Poppins', sans-serif;
 `
 
+export const MessageInput = styled.textarea`
+background-color: #e7e2e2;
+width: 78%;
+height: 30px;
+border-radius: 12px;
+border: 4px solid #839bcd;
+box-shadow: 0;
+outline:0;
+margin: .5rem 0;
+font-size: 1rem;
+font-family: 'Poppins', sans-serif;
+height: 20vh;
+overflow-wrap: break-word;
+padding: 1%;
+`
+
+export const SubmitButton = styled.button`
+  width: 20%;
+  height: 7.5vh;
+  margin: 1rem 0;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  font-size: 1rem;
+  font-family: 'Poppins', sans-serif;
+
+  background-color: #32d22d;
+
+  border-radius: 12px;
+`
+
 export const Form = styled.form`
 background-color: none;
 width: 100%;
